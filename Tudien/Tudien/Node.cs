954eda53a46bb711239ed801eae363c3f5a25dab
phan duong pha <15110094@student.hcmute.edu.vn>
2017-10-17 16:30:06 +0700

    next3

---
+++ Tudien/Tudien/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tudien
+{
+   public class Node
+    {
+
+        public TUDIEN Data;
+        public Node Next;
+
+        //public Node[] Bucket = new Node[26];
+
+        public Node(TUDIEN data, Node next)
+        {
+            this.Data = Data;
+            this.Next = Next;
+        }
+        public Node()
+        {
+            this.Data = null;
+            this.Next = null;
+        }
+        public TUDIEN data
+        {
+            get { return Data; }
+            set { Data = value; }
+        }
+        public Node next
+        {
+            get { return Next; }
+            set { Next = value; }
+        }
+    }
+}
--- Tudien/Tudien/fmain.cs
+++ Tudien/Tudien/fmain.cs
@@ -25,70 +25,76 @@ namespace Tudien
 
         }
         //tạo class từ điển
-       class TUDIEN
-        {
-            public char[] Tu; //từ
-            public char[] Nghia;//nghĩa của từ 
-            public char[] LoaiTu;//loại từ
+        //class TUDIEN
+        // {
+        //     public char[] Tu; //từ
+        //     public char[] Nghia;//nghĩa của từ 
+        //     public char[] LoaiTu;//loại từ
 
-            public TUDIEN(char[] Tu, char[] Nghia, char[] LoaiTu)
-            {
-                this.Tu = Tu;
-                this.Nghia = Nghia;
-                this.LoaiTu = LoaiTu;
-            }
-            public char[] tu
-            {
-                get { return Tu; }
-            }
-            public char[] nghia
-            {
-                get { return Nghia; }
-            }
-            public char[] loaitu
-            {
-                get { return LoaiTu; }
-            }
-        }
+        //     public TUDIEN(char[] Tu, char[] Nghia, char[] LoaiTu)
+        //     {
+        //         this.Tu = Tu;
+        //         this.Nghia = Nghia;
+        //         this.LoaiTu = LoaiTu;
+        //     }
+        //     public char[] tu
+        //     {
+        //         get { return Tu; }
+        //     }
+        //     public char[] nghia
+        //     {
+        //         get { return Nghia; }
+        //     }
+        //     public char[] loaitu
+        //     {
+        //         get { return LoaiTu; }
+        //     }
+        // }
 
         //tiếp theo là định nghĩa một node
-        class Node
-        {
-            public TUDIEN Data;
-            public Node Next;
+        //class Node
+        //{
+        //    public TUDIEN Data;
+        //    public Node Next;
 
-            public Node[] Bucket=new Node[26];
-            
-            public Node(TUDIEN data, Node next)
-            {
-                this.Data = Data;
-                this.Next = Next;
-            }
-            public Node()
-            {
-                this.Data = null;
-                this.Next = null;
-            }
-            public TUDIEN data
-            {
-                get { return Data; }
-                set { Data = value; }
-            }
-            public Node next
-            {
-                get { return Next; }
-                set { Next = value; }
-            }
-        }
+        //    public Node[] Bucket=new Node[26];
+
+        //    public Node(TUDIEN data, Node next)
+        //    {
+        //        this.Data = Data;
+        //        this.Next = Next;
+        //    }
+        //    public Node()
+        //    {
+        //        this.Data = null;
+        //        this.Next = null;
+        //    }
+        //    public TUDIEN data
+        //    {
+        //        get { return Data; }
+        //        set { Data = value; }
+        //    }
+        //    public Node next
+        //    {
+        //        get { return Next; }
+        //        set { Next = value; }
+        //    }
+        //}
 
         // Khởi tạo thùng bucket 
 
+            //
+        public Node[] Bucket = new Node[26];
         void Initialize()
         {
             for (int b = 0; b <26; b++)
-                Bucket[b] = NULL;
+                Bucket[b] = null;
         }
-
+        //kiem tra bucket b co rong khong
+        bool EmptyBucket(int b)
+        {
+            return (Bucket[b] == null ? true : false);
+        }
         //hàm băm
         int Hashfunc(char[] Tu)
         {
